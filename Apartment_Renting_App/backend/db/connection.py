5e3d921acdd1080c90310b1e44fd3037fc6992e5
sushilkplassar <sushilkplassar@gmail.com>
2019-05-11 17:48:49 -0700

    COnfilict resolution in connection.py file while merging code with frontend and backend

--- Apartment_Renting_App/backend/db/connection.py
+++ Apartment_Renting_App/backend/db/connection.py
@@ -8,17 +8,10 @@ cursor = conn.cursor()
 
 
 # conn = mysql.connector.connect(host = "localhost",
-<<<<<<< HEAD
-#                            database = 'CSC648',
-#                            user = 'root',
-#                            password = 'Derectorjin1',
-#                            auth_plugin='mysql_native_password')
-=======
 #                             database = 'CSC648',
 #                             user = 'root',
 #                             password = 'Derectorjin1',
 #                             auth_plugin='mysql_native_password')
->>>>>>> origin/brians_frontend
 # cursor = conn.cursor()
 
 
